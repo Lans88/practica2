a629b481dd0d3539c9740bc5c10aa4d715a43790
Artem <art001.163@gmail.com>
2022-12-29 23:38:34 +0400

    create autotest

---
+++ src/main/java/ru/netology/practica2/MaxService.java
@@ -0,0 +1,11 @@
+package ru.netology.practica2;
+
+public class MaxService {
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
